d299247ee5cd03f4adefbaab8a0bd90523a575b2
Guillermo Ortega Valladares <guillermortega1e@gmail.com>
2019-07-21 20:51:43 -0500

    (test) add another test case, generates error

--- tests/Unit/ProblemOneTest.php
+++ tests/Unit/ProblemOneTest.php
@@ -9,5 +9,6 @@ class ProblemOneTest extends TestCase {
 
     $this->assertTrue( ProblemOne::get( [ 1, 2, 3, 4, 5 ] ) === [ 120, 60, 40, 30, 24 ] );
     $this->assertTrue( ProblemOne::get( [ 3, 2, 1 ] ) === [ 2, 3, 6 ] );
+    $this->assertTrue( ProblemOne::get( [ 3, 2, 3 ] ) === [ 6, 9, 6 ] );
   }
 }
