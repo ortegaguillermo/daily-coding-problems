831eec22560aede946715e89ad4cfd38e36ef2b3
Guillermo <guillermortega1e@gmail.com>
2021-03-25 20:03:02 -0600

    Update ProblemOneTest.php
    add test cases via dataProvider

--- tests/Unit/ProblemOneTest.php
+++ tests/Unit/ProblemOneTest.php
@@ -4,11 +4,38 @@ use App\Http\Controllers\Problems\ProblemOne;
 use Tests\TestCase;
 
 class ProblemOneTest extends TestCase {
+  
+  public function dataSource(){
+    
+    $input1 = [1, 2, 3, 4, 5];
+    $expectedOutput1 = [ 120, 60, 40, 30, 24 ];
+    
+    $input2 = [3, 2, 1];
+    $expectedOutput2 = [2, 3, 6];
+    
+    $input3 = [3, 2, 3];
+    $expectedOutput3 = [6, 9, 6];
+    
+    return [
+      'test-case-1'=>[ $input1, $expectedOutput1 ],
+      'test-case-2'=>[ $input2, $expectedOutput2 ],
+      'test-case-3'=>[ $input3, $expectedOutput3 ],
+    ];
+  }
 
-  public function testGet() {
+  /**
+  * @param array $input
+  * @param array $expectedOutput
+  */
+  public function testGet(array $input, array $expectedOuptut) {
 
-    $this->assertTrue( ProblemOne::get( [ 1, 2, 3, 4, 5 ] ) === [ 120, 60, 40, 30, 24 ] );
-    $this->assertTrue( ProblemOne::get( [ 3, 2, 1 ] ) === [ 2, 3, 6 ] );
-    $this->assertTrue( ProblemOne::get( [ 3, 2, 3 ] ) === [ 6, 9, 6 ] );
+    $ouptut = ProblemOne::get( $input );
+    
+    $this->assertSame(
+      $expectedOutput, 
+      $output, 
+      'The output was not the expected, the behivior does not match the expected criteria'
+    );
+    
   }
 }
