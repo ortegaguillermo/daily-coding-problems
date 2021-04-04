8f3bb8891d3647bb854948874aea503a5bfb2b4f
Guillermo Ortega Valladares <guillermortega1e@gmail.com>
2021-04-04 04:09:50 -0500

    fix: typos

--- tests/Unit/ProblemOneTest.php
+++ tests/Unit/ProblemOneTest.php
@@ -3,41 +3,46 @@
 use App\Http\Controllers\Problems\ProblemOne;
 use Tests\TestCase;
 
-class ProblemOneTest extends TestCase {
-  
-  public function dataProviderFunction(){
-    
-    $input1 = [1, 2, 3, 4, 5];
-    $expectedOutput1 = [ 120, 60, 40, 30, 24 ];
-    
-    $input2 = [3, 2, 1];
-    $expectedOutput2 = [2, 3, 6];
-    
-    $input3 = [3, 2, 3];
-    $expectedOutput3 = [6, 9, 6];
-    
-    return [
-      'test-case-1'=>[ $input1, $expectedOutput1 ],
-      'test-case-2'=>[ $input2, $expectedOutput2 ],
-      'test-case-3'=>[ $input3, $expectedOutput3 ],
-    ];
-    
-  }
+class ProblemOneTest extends TestCase
+{
 
-  /**
-  * @dataProvider dataProviderFunction
-  * @param array $input
-  * @param array $expectedOutput
-  */
-  public function testGet(array $input, array $expectedOutput) {
+    public function dataProviderFunction(): array
+    {
 
-    $output = ProblemOne::get( $input );
-    
-    $this->assertSame(
-      $expectedOutput, 
-      $output, 
-      'The output was not the expected, the behivior does not match the expected criteria'
-    );
-    
-  }
+        $input1 = [1, 2, 3, 4, 5];
+        $expectedOutput1 = [120, 60, 40, 30, 24];
+
+        $input2 = [3, 2, 1];
+        $expectedOutput2 = [2, 3, 6];
+
+        $input3 = [3, 2, 3];
+        $expectedOutput3 = [6, 9, 6];
+
+        return [
+            'test-case-1' => [$input1, $expectedOutput1],
+            'test-case-2' => [$input2, $expectedOutput2],
+            'test-case-3' => [$input3, $expectedOutput3],
+        ];
+
+    }
+
+    /**
+     * @dataProvider dataProviderFunction
+     * @param array $input
+     * @param array $expectedOutput
+     *
+     * @return void
+     */
+    public function testGet(array $input, array $expectedOutput)
+    {
+
+        $output = ProblemOne::get($input);
+
+        $this->assertSame(
+            $expectedOutput,
+            $output,
+            'The output was not the expected, the behavior does not match the expected criteria'
+        );
+
+    }
 }
